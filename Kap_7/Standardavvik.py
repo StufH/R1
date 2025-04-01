4f4472f2bac8aaab990026f4eba80659be6031ed
Henrik Stueland <henrikstueland@gmail.com>
2025-04-01 12:51:53 +0200

    Update Standardavvik.py

--- Kap_7/Standardavvik.py
+++ Kap_7/Standardavvik.py
@@ -1,9 +1,24 @@
 from pylab import *
 
+# Data:
 y = array([3.73, 3.53, 3.75, 3.63, 3.82])
 n = len(y)
+
+# Beregning av snitt og standardavvik:
 y_snitt = sum(y) / n
 s = sqrt(sum((y - y_snitt)**2) / (n-1))
 
-print(f"gjennomsnittet er: {y_snitt}")
-print(f"standardavviket er: {s}")
+# beregning av standardfeil:
+se = s / sqrt(n)
+
+print(f"gjennomsnittet er: {round(y_snitt, 3)}")
+print(f"standardavviket er: {round(s, 3)}")
+print(f"standardfeilen er: {round(se, 3)}")
+
+#plotting av data:
+plot(y,y_snitt, "b.")
+vlines(y, y_snitt - 2 * se, y_snitt + 2 * se, "r")
+xlabel("Måling")
+ylabel("Verdi")
+title("Standardavvik")
+show()
